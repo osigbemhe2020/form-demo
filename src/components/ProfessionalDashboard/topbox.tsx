de9c449a300dfdda2cc21bad35789cc9e8045689
osigbemhe2020 <62507389+osigbemhe2020@users.noreply.github.com>
2025-10-08 14:19:42 +0100

    Update topbox.tsx

--- src/components/ProfessionalDashboard/topbox.tsx
+++ src/components/ProfessionalDashboard/topbox.tsx
@@ -1,14 +1,12 @@
+import Image from "next/image";
 import { FaCamera } from 'react-icons/fa';
 
 const Topbox = () => {
   return (
     <div className="w-[102%] lg:h-[178px] bg-white px-4 sm:px-6 lg:px-[60px] py-3 flex flex-col lg:flex-row items-center lg:items-center gap-4">
       <div className="relative flex-shrink-0">
-        <img
-          src="/images/windows.jpeg"
-          alt="placeholder"
-          className="w-24 h-24 sm:w-[150px] sm:h-[150px] border-2 border-[#006A05] rounded-full object-cover"
-        />
+     <Image src="/images/profile-image.jpg" alt="placeholder" width={150} height={150} 
+          className="w-24 h-24 sm:w-[150px] sm:h-[150px] border-2 border-[#006A05] rounded-full object-cover"/>
         {/* Camera icon overlay */}
         <div className="absolute bottom-1 right-4 translate-x-1/4 translate-y-1/4 w-8 h-8 bg-[#006A05] rounded-full flex items-center justify-center">
           <FaCamera className="text-white text-sm" />
